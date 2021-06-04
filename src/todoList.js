def5997ffc7eb622760b8fb9c63daf52b23067ca
geshan99 <geshansudasinghe@gmail.com>
2021-06-05 03:31:24 +0530

    Update todoList.js

--- src/todoList.js
+++ src/todoList.js
@@ -3,7 +3,7 @@ const todoList = [
   { id: 2, text: "This is the task you want to do", completed: true },
   { id: 3, text: "This is the task you want to do", completed: false },
   { id: 4, text: "This is the task you want to do", completed: true },
-  { id: 5, text: "This is the task you want to do", completed: false },
+  { id: 5, text: "This is the task you want to do5", completed: false },
 ];
 
 export default todoList;
